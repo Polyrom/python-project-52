a2f41ae32aff53fcdb10a4db3ec45ebd3e1d6566
Roman Poliakov <roman61394@gmail.com>
2022-10-12 19:13:35 +0400

    optimize template layout | add login required custom message

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.views.generic.edit import DeleteView, UpdateView
 from users.forms import UserCreateForm
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 
 class UsersListView(ListView):
@@ -19,17 +21,30 @@ class SignupView(CreateView):
         return '/login'
 
 
-class UserUpdateView(SuccessMessageMixin, UpdateView):
+class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserCreateForm
-    success_url = '/users'
     template_name = 'users/user_update.html'
+    success_url = '/users/'
+    login_url = '/login/'
     success_message = 'Пользователь успешно изменён'
+    permission_denied_message = 'Вы не авторизованы! Пожалуйста, выполните вход.'
+    redirect_field_name = ''
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.add_message(request, messages.ERROR,
+                                 self.permission_denied_message)
+            return self.handle_no_permission()
+        return super().dispatch(
+            request, *args, **kwargs
+        )
 
 
-class UserDeleteView(SuccessMessageMixin, DeleteView):
+class UserDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = User
     success_url = '/users'
     template_name = 'users/user_delete.html'
+    login_url = '/login/'
     success_message = 'Пользователь успешно удалён'
 
