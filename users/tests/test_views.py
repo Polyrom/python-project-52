8b00a7d309c9d89f619fa3d9484b804f9dfe1b30
Roman Poliakov <roman61394@gmail.com>
2022-10-14 01:42:17 +0400

    add post method tests for user update/delete views

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -8,13 +8,16 @@ class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
+
         self.user = User.objects.create(
             pk=1,
             username='Boss',
             first_name='Michael',
             last_name='Scott'
         )
-        self.client.force_login(user=self.user)
+        self.user.set_password('12345')
+        self.user.save()
+
         self.users_list = reverse('users.list')
         self.create_user = reverse('user.create')
         self.delete_user = reverse('user.delete', kwargs={'pk': 1})
@@ -30,12 +33,47 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/user_create.html')
 
+    def test_signup_POST(self):
+        data = {
+            'first_name': 'Dwight',
+            'last_name': 'Shrute',
+            'username': 'BestSalesman',
+            'password1': 'scranton',
+            'password2': 'scranton'
+        }
+        response = self.client.post(self.create_user, data=data)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(User.objects.count(), 2)
+
+    def test_update_user_GET_correct_user(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get(self.update_user)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/user_update.html')
+
+    def test_update_user_GET_wrong_user(self):
+        response = self.client.get(self.update_user)
+        self.assertEquals(response.status_code, 302)
+
+    def test_update_user_POST(self):
+        self.client.force_login(user=self.user)
+        data = {
+            'first_name': 'Michael',
+            'last_name': 'Scott',
+            'username': 'RegionalManager',
+            'password1': '12345',
+            'password2': '12345'
+        }
+        response = self.client.post(self.update_user, data=data)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(User.objects.first().username, 'RegionalManager')
+
     def test_delete_user_GET(self):
         response = self.client.get(self.delete_user)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/user_delete.html')
 
-    def test_update_user_GET(self):
-        response = self.client.get(self.update_user)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/user_update.html')
+    def test_delete_user_POST(self):
+        response = self.client.delete(self.delete_user)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(User.objects.count(), 0)
