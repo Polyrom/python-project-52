a713318b9294761e64fc051b3b2ca98310549e7d
Roman Poliakov <roman61394@gmail.com>
2022-10-21 00:45:39 +0400

    update urlpaths | update pyproject.toml

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from users.views import UsersListView, SignupView, UserDeleteView, UserUpdateVie
 urlpatterns = [
     path('', UsersListView.as_view(), name='users.list'),
     path('create/', SignupView.as_view(), name='user.create'),
-    path('<int:pk>/delete', UserDeleteView.as_view(), name='user.delete'),
-    path('<int:pk>/update', UserUpdateView.as_view(), name='user.update'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user.delete'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user.update'),
 ]
